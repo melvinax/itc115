ccfdd0228bfbc52d317dc0a99ee37c1bf5d3f231
Melvina <35507459+melvinax@users.noreply.github.com>
2018-10-01 15:11:49 -0700

    assignment 01

---
+++ assignment01/src/Hello.java
@@ -0,0 +1,12 @@
+
+public class Hello {
+
+	public static void main(String[] args) {
+		System.out.println("Hello, world!");
+		System.out.println();
+		System.out.println("This is an additional line");
+		// TODO Auto-generated method stub
+
+	}
+
+}
